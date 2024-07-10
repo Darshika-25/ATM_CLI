321e9c141c8661d75ba2eba6824634a048bd884e
Darshika-25 <166216466+Darshika-25@users.noreply.github.com>
2024-07-10 19:52:20 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+This TypeScript Node project is a command-line interface (CLI) application that simulates an ATM machine. Users can interact with the ATM by entering commands to check their balance and  withdraw funds. 
+
+To use the ATM machine CLI, simply install the package globally using npm and run the command atm machine in your terminal. 
+npm i @darshika.dev/atm_cli
+// npx @darshika.dev/atm_cli
+
+You will be prompted to enter your PIN to access your account and begin using the ATM features.
+PIN Code: 1025
+
+
+
+
---
+++ index.ts
@@ -0,0 +1,46 @@
+#! /usr/bin/env node
+// shebang for npm execution
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let myBalance = 10000; // Initial balance in dollars
+let myPin = 1025; // Predefined PIN code
+
+const pinCode = await inquirer.prompt([
+  {
+    name: "pin",
+    type: "number",
+    message: "Enter your PIN:",
+  },
+]);
+
+if (pinCode.pin === myPin) {
+  console.log("Correct PIN code");
+
+  const operationAns = await inquirer.prompt({
+    name: "operation",
+    type: "list",
+    message: "Please select an option:",
+    choices: ["Withdraw", "Check Balance"],
+  });
+
+  if (operationAns.operation === "Withdraw") {
+    const amountAns = await inquirer.prompt({
+      name: "amount",
+      message: "Enter the amount you want to withdraw:",
+      type: "number",
+    });
+
+    if (amountAns.amount > myBalance) {
+      console.log(chalk.bold.red("You have insufficient balance."));
+    } else {
+      myBalance -= amountAns.amount;
+      console.log(chalk.blueBright("Your remaining balance is $" + myBalance));
+    }
+  } else if (operationAns.operation === "Check Balance") {
+    console.log(chalk.blueBright("Your current balance is $" + myBalance));
+  }
+} else {
+  console.log(chalk.bold.green("Incorrect PIN number"));
+}
